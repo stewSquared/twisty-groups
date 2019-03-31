768332598aeb86fa9ed39d38ffe08c9a136da679
Stewart Stewart <stewinsalot@gmail.com>
2019-03-30 23:19:23 -0400

    Add some helpers for decomposing a Perm

---
+++ src/main/scala/cube/package.scala
@@ -0,0 +1,32 @@
+import net.alasc.perms._
+
+import cats.kernel.Group
+import cats.syntax.group._
+
+package object cube {
+  def isEven(perm: Perm) =
+    ((perm.toCycles.seq.length % 2) ^ (perm.nMovedPoints % 2)) == 0
+
+  def asTranspositions(perm: Perm): List[Perm] = asTranspositionsTailRec(perm, Nil)
+
+  private def asTranspositionsTailRec(perm: Perm, transpositions: List[Perm]): List[Perm] = {
+    perm.smallestMovedPoint.fold(transpositions) { smallest =>
+      val t = Perm.transposition(perm.invImage(smallest), smallest)
+      asTranspositionsTailRec(perm |-| t, t :: transpositions)
+    }
+  }
+
+  def asThreeCycles(perm: Perm): Option[List[Perm]] = {
+    if (!isEven(perm)) None else Some {
+      val swaps = if (perm.toCycles.seq.length <= 1) {
+        asTranspositions(perm)
+      } else {
+        val cycleSmallestPoints = perm.toCycles.seq.map(_.seq.min)
+        val crossPerm = Perm(perm.toCycles.seq.map(_.seq.min): _*)
+        asTranspositions(perm |+| crossPerm) ++ asTranspositions(crossPerm.inverse)
+      }
+
+      swaps.grouped(2).map(Group.combineAll(_)).toList
+    }
+  }
+}
